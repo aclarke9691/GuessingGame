1f6cff022b39fd2d13d34dcb7914ed9735041c67
BT_1N3_29@BKTECH.LOCAL <BT_1N3_29@BKTECH.LOCAL>
2018-10-10 14:48:32 -0400

    Initial commit

---
+++ src/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+
+    public static void guessingGame(int n)
+    {
+        import java.util.scanner;
+        Scanner input = newScanner(system.in);
+        n= (int) Math.random();
+        n= n*101;
+        int guess= 0;
+        System.out.print("I'm thinking of a number from 1-100. Guess it.");
+        while (!guess.equals(n))
+    }
+}
